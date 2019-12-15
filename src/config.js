b39aa619e42e06fb504a47303d06f9d85ee24f94
DarkThor76 <k9fan777@gmail.com>
2019-12-15 04:01:54 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "rmvjason-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://d3sxivgx2c.execute-api.us-east-2.amazonaws.com/prod/"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_SFOd2vUmc",
+    APP_CLIENT_ID: "69ghtkjqdev8guvvsmdpb3qigh",
+    IDENTITY_POOL_ID: "us-east-2:96520cd7-106c-4099-8d3d-001f7c895876"
+  }
+};
